dbe8935d706660b0e48163fec63430278facf12f
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-11-25 14:58:11 +0700

    Reorganize the repo to meet the required structure of PA0

---
+++ src/BackEnd/PageRhythm/app.py
@@ -0,0 +1,13 @@
+from routes.home import home_blueprint
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+app.register_blueprint(home_blueprint)
+
+if __name__ == "__main__":
+
+    app.run(debug = True)
+
+
+
