dc7b89f4a6df4a238166b8377f5f40b392f54c8b
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-11-25 08:08:54 +0700

    Add requirements.txt

--- BackEnd/PageRhythm/main.py
+++ BackEnd/PageRhythm/main.py
@@ -1,9 +1,23 @@
 import os
 from dotenv import load_dotenv
+from flask import Flask, request, jsonify
 from supabase import create_client, Client
 
-load_dotenv()
+app = Flask(__name__)   
 
-url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+def createSupabaseClient() -> Client:
+    load_dotenv()
+
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+    supabase: Client = create_client(url, key)
+    return supabase
+
+@app.route("/")
+def home():
+    return jsonify({"message": "Welcome to PageRhythm!"}), 200
+
+if __name__ == "__main__":
+
+    supabase = createSupabaseClient()
+    app.run(debug = True)
