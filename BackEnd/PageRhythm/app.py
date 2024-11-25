761191ac9485cc1dec5051760e99ac1d20bf7668
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-11-25 09:24:13 +0700

    Register first blueprint

--- BackEnd/PageRhythm/app.py
+++ BackEnd/PageRhythm/app.py
@@ -1,9 +1,12 @@
 import os
 from dotenv import load_dotenv
+from routes.home import home_blueprint
 from flask import Flask, request, jsonify
 from supabase import create_client, Client
 
 app = Flask(__name__)   
+
+app.register_blueprint(home_blueprint)
 
 def createSupabaseClient() -> Client:
     load_dotenv()
@@ -12,10 +15,6 @@ def createSupabaseClient() -> Client:
     key: str = os.environ.get("SUPABASE_KEY")
     supabase: Client = create_client(url, key)
     return supabase
-
-@app.route("/")
-def home():
-    return jsonify({"message": "Welcome to PageRhythm!"}), 200
 
 if __name__ == "__main__":
 
