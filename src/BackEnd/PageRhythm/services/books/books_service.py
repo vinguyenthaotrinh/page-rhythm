fc69292c25d95ef5480dc4cdc2880a4e081f7490
Trịnh Nguyễn Thảo Vi <tntvi22@apcs.fitus.edu.vn>
2024-12-01 01:41:33 +0700

    remove inconsistent file name

--- src/BackEnd/PageRhythm/services/books/books_service.py
+++
@@ -1,6 +0,0 @@
-from services.books.supabase_books_api_service import SupabaseBooksAPIService
-
-class BooksService:
-
-    def __init__(self):
-        self.supabase = SupabaseBooksAPIService
--- src/BackEnd/PageRhythm/services/books/supabase_books_api_service.py
+++
@@ -1,6 +0,0 @@
-from services.supabase_client_service import SupabaseClientService
-
-class SupabaseBooksAPIService:
-
-    def __init__(self):
-        self.client = SupabaseClientService()
