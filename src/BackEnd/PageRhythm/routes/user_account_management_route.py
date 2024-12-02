63727aea87a6421510f1a48d4872ec40356946f6
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-12-02 08:26:11 +0700

    Fix bug in setting JWT

--- src/BackEnd/PageRhythm/routes/user_account_management_route.py
+++ src/BackEnd/PageRhythm/routes/user_account_management_route.py
@@ -1,3 +1,5 @@
 from flask import Blueprint, jsonify, request
 from services.authentication.authentication_service import AuthenticationService
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
+user_account_management_blueprint = Blueprint("user_account_management", __name__)
