c5df2da4bbe6c3159db1289237019d326f85db1e
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-12-05 22:16:17 +0700

    Add login section

--- src/FrontEnd/PageRhythm/src/components/LandingPage.tsx
+++ src/FrontEnd/PageRhythm/src/components/LandingPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import IMAGES from "../images"; // Ensure IMAGES.LOGO is imported correctly
+import IMAGES from "../images";
 import "../styles/landing-page-styles.css";
 
 function LogoSection() {
@@ -11,10 +11,20 @@ function LogoSection() {
     );
 }
 
+function LoginSection() {
+    return (
+        <div id="login-section">
+            <img src={IMAGES.LANDING_PAGE_LOGIN_SECTION_TOP_RIGHT_CORNER}   className="overlay-image" id = "login-section-top-right-corner-image" />
+            <img src={IMAGES.LANDING_PAGE_LOGIN_SECTION_BOTTOM_LEFT_CORNER} className="overlay-image" id = "login-section-bottom-left-corner-image" />
+        </div>
+    )
+}
+
 export default function LandingPage() {
     return (
         <div id="landing-page">
             <LogoSection />
+            <LoginSection />
         </div>
     );
 }
--- src/FrontEnd/PageRhythm/src/images.ts
+++ src/FrontEnd/PageRhythm/src/images.ts
@@ -1,7 +1,11 @@
 import LOGO from "./assets/images/logo.png";
+import LANDING_PAGE_LOGIN_SECTION_BOTTOM_LEFT_CORNER from "./assets/images/landing-page-login-section-bottom-left-corner.png";
+import LANDING_PAGE_LOGIN_SECTION_TOP_RIGHT_CORNER from "./assets/images/landing-page-login-section-top-right-corner.png";
 
 const IMAGES = {
-    LOGO
+    LOGO,
+    LANDING_PAGE_LOGIN_SECTION_BOTTOM_LEFT_CORNER,
+    LANDING_PAGE_LOGIN_SECTION_TOP_RIGHT_CORNER
 };
 
 export default IMAGES;
