d662945ab816b9b154c9b2c9aa06f6eac5078f72
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-12-05 21:56:53 +0700

    Add style to landing page

--- src/FrontEnd/PageRhythm/src/App.tsx
+++ src/FrontEnd/PageRhythm/src/App.tsx
@@ -1,18 +1,14 @@
-import React from 'react';
-import './App.css';
-import LandingPage from './components/LandingPage'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import LandingPage from "./components/LandingPage";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 export default function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={
-          <div className="App">
-            <LandingPage />
-          </div>
-        } />
+        <Route path="/" element={<LandingPage />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
--- src/FrontEnd/PageRhythm/src/images.ts
+++ src/FrontEnd/PageRhythm/src/images.ts
@@ -1,31 +1,7 @@
-import logo1 from "./assets/images/logo.png";
-import bg_img1 from "./assets/images/bottom_right_corner.png";
-import bg_img2 from "./assets/images/bottom_left_corner.png";
-import bg_img3 from "./assets/images/ellipse.png";
-import bg_img4 from "./assets/images/top_right_corner.png";
-import bg_img5 from "./assets/images/top_left_corner.png";
-import lock from "./assets/images/lock.png";
-import user from "./assets/images/user.png";
-import mail from "./assets/images/mail.png";
-import pencil from "./assets/images/pencil.png";
-import calendar from "./assets/images/calendar.png";
-import search from "./assets/images/search.png";
-import google from "./assets/images/google.png";
+import LOGO from "./assets/images/logo.png";
 
-const images = {
-    logo1,
-    bg_img1,
-    bg_img2,
-    bg_img3,
-    bg_img4,
-    bg_img5,
-    lock,
-    user,
-    mail,
-    pencil,
-    calendar,
-    search,
-    google,
+const IMAGES = {
+    LOGO
 };
 
-export default images;
+export default IMAGES;
