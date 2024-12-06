f1b7e68b102623a8089d197c9e6b49c2bbf0ef83
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-12-06 21:39:50 +0700

    Modify navigation bar

--- src/FrontEnd/PageRhythm/src/components/NavigationBar.tsx
+++ src/FrontEnd/PageRhythm/src/components/NavigationBar.tsx
@@ -2,30 +2,47 @@ import React from "react"
 import IMAGES from "../images"
 import "../styles/fonts.css"
 import "../styles/navigation-bar-styles.css"
-import { Link, NavLink } from "react-router-dom"
+import { Link, NavLink, useNavigate } from "react-router-dom"
+
+function LogoSectionOfNavigationBar() {
+    const navigate = useNavigate();
+
+    const handleLogoClick = () => {
+        navigate("/home-page");
+    };
+
+    return (
+        <div 
+            className="navigation-bar-left"
+            role="button"
+            tabIndex={0}
+            onClick={handleLogoClick}
+            onKeyDown={(e) => e.key === 'Enter' && handleLogoClick()}
+            style={{ cursor: 'pointer' }}
+        >
+            <img src={IMAGES.LOGO} alt="PageRhythm Logo" className="navigation-bar-logo" />
+            <h1 className="navigation-bar-title">PageRhythm</h1>
+        </div>
+    );
+}
 
 export default function NavigationBar() {
    return(
        <>
-           
-               <nav>
-                   <div className="navbar-left">
-                       <img src={IMAGES.LOGO} alt="PageRhythm Logo" className="navbar-logo" />
-                       <h1 className="navbar-title">PageRhythm</h1>
-                   </div>
-                   <div className="navbar-right">
-                       <ul>
-                           <li>
-                               <NavLink to="/">Home</NavLink>
-                           </li>
-                           <li>
-                               <NavLink to="/mylibrary">My Library</NavLink>
-                           </li>
-                       </ul>
-                       <img src={IMAGES.LOGO} alt="User Avatar" className="navbar-avatar" />
-                   </div>
-               </nav>
-           
+           <nav>
+                <LogoSectionOfNavigationBar />
+                <div className="navigation-bar-right">
+                   <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/mylibrary">My Library</NavLink>
+                        </li>
+                    </ul>
+                    <img src={IMAGES.LOGO} alt="User Avatar" className="navigation-bar-avatar" />
+                </div>
+            </nav>
        </>
    )
 }
