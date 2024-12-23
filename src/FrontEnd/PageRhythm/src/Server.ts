64ba170326db71c514933461f1a36aa1bd6ba4d5
Nguyễn Hoàng Minh <95891385+nguyenhoangminh31082003@users.noreply.github.com>
2024-12-23 14:51:01 +0700

    Turn off debug mode to prepare deployment

--- src/FrontEnd/PageRhythm/src/Server.ts
+++ src/FrontEnd/PageRhythm/src/Server.ts
@@ -8,7 +8,7 @@ export default class Server {
         if (Server.instance === null) {
             Server.instance = new Server();
             await Server.instance.initializeHost(
-                null,//"https://page-rhythm-back-end.onrender.com", 
+                "https://page-rhythm-back-end.onrender.com", 
                 "http://127.0.0.1:5000"
             );
         }
